3583b56535f67138ccdcfb78742f9a7889eb3c88
JH <pioneer3692@gmail.com>
2023-01-18 12:57:24 +0900

    modified error for distribution for slots

--- cor/common.py
+++ cor/common.py
@@ -29,20 +29,72 @@ def get_param_dict(url):
     params = parse_qs(urlsplit(url).query)
     return {k:v[0] if v else None for k,v in params.items()}
 
-def get_non_duplicated_dict_list(original_list: List[Dict]) -> List[List[Dict]]:
+def get_non_duplicated_dict_list(original_dict: Dict[List]) -> List[List[Dict]]:
     """_summary_
 
     Args:
-        original_list (_type_): _description_ [{key: val, ....}, {key: val, ....},...]
-        -> {val1: [{val2: val3}, ...], val1: [{val2: val3, ...}, ...]}
+        original_dict (_type_): _description_ [{key: val, ....}, {key: val, ....},...]
+        Returns:
+        _type_: _description_[[{key: val, ....},....][{key: val, ....},....],..]
     """
-    CONCURRENCY_MAX = 10
-    vendor_dict = {}
-    for item in original_list:
+    CONCURRENCY_MAX = 10    
+    list_chunks = []
+    test_chunk = []
+    test_list_chunks = []
+    i = 0    
+    urls = list(original_dict.keys())    
+    while len(original_dict.items()):
+        url = urls[i]
+        try:
+            value = original_dict[url]
+        except KeyError:
+            continue    
+        if not value:
+            del(original_dict[url])
+            if len(urls) - 1 == i:        
+                i = 0
+            else:
+                i += 1
+            i += 1
+            continue
+        try:
+            re.match(r'https.+', url).group()
+        except AttributeError:
+            continue
+        residue = re.sub(r'\?.+', '', url)
+        res = get_param_dict(url)
+        product_id = re.sub(r'[^0-9]+', '', residue)
+        res['productId'] = product_id
+        if len(test_chunk) >= CONCURRENCY_MAX:
+            test_list_chunks.append(test_chunk)
+            test_chunk = []
+        
+        val = original_dict[url].pop()    
+        sid = list(val.keys())
+        keyword = list(val.values())
+        try:
+            test_chunk.append({'id': sid[0], 'keyword':keyword[0], 'product_id': product_id, 'item_id': res['itemId'], 'vendor_item_id': res['vendorItemId']})
+        except KeyError:
+            test_chunk.append({'id': sid[0], 'keyword':keyword[0], 'product_id': product_id, 'item_id': res['vendorItemId'], 'vendor_item_id': res['vendorItemId']})
+        
+    return list_chunks
+
+
+def preprocess(result:List[Dict])->Dict[List]:
+    """_summary_
+
+    Args:
+        result (_type_): _description_ [{key1: val1, key2: val2, ....}, {key1: val1, key2: val2, ....}, {key1: val1, key2: val2, ....}]
+        
+    Returns:
+        _type_: _description_ {val1: [{val2: val3}, ...], val1: [{val2: val3, ...}, ...]}
+    """
+    vendor_dict = {}            
+    for item in result:
         try:
             item['p_url'] = re.sub(r' + ', '', item['p_url'])
             re.match(r'\S*.*https://.+', item['p_url']).group()
-        except AttributeError:        
+        except AttributeError:            
             continue
         try:
             item['p_url'] = re.search(r'https://.+', item['p_url']).group()
@@ -52,40 +104,9 @@ def get_non_duplicated_dict_list(original_list: List[Dict]) -> List[List[Dict]]:
         try:
             vendor_dict[item['p_url']]
         except KeyError:
-            vendor_dict[item['p_url']] = []
-        
+            vendor_dict[item['p_url']] = []        
         vendor_dict[item['p_url']].append({item['id']: item['Keyword']})
-
-        
-    data = OrderedDict(sorted(vendor_dict.items(), key=lambda x: len(x[1]), reverse=random.choice([True, False])))
-    list_chunks = []
-    chunk = []
-    i = 0    
-    while i < len(data):
-        url, val = list(data.items())[i]
-        try:
-            re.match(r'https.+', url).group()
-        except AttributeError:
-            continue
-        residue = re.sub(r'\?.+', '', url)
-        res = get_param_dict(url)
-        product_id = re.sub(r'[^0-9]+', '', residue)
-        res['productId'] = product_id
-        if len(chunk) >= CONCURRENCY_MAX:
-            list_chunks.append(chunk)
-            chunk = []
-        try:           
-            val = val.pop()
-        except IndexError:
-            pass
-        sid = list(val.keys())
-        keyword = list(val.values())
-        try:
-            chunk.append({'id': sid[0], 'keyword':keyword[0], 'product_id': product_id, 'item_id': res['itemId'], 'vendor_item_id': res['vendorItemId']})
-        except KeyError:
-            chunk.append({'id': sid[0], 'keyword':keyword[0], 'product_id': product_id, 'item_id': res['vendorItemId'], 'vendor_item_id': res['vendorItemId']})
-        i += 1
-    return list_chunks
+    return vendor_dict   
 
 def platform_info():
     lines = []
--- cor/slotdata.py
+++ cor/slotdata.py
@@ -1,4 +1,5 @@
 import asyncio
+from collections import OrderedDict
 import datetime
 import datetime
 import json
@@ -11,7 +12,17 @@ import random
 from aiohttp import ClientSession
 import asyncio
 
+from concurrent.futures import ThreadPoolExecutor
+write_executor = ThreadPoolExecutor(max_workers=1)
+
+def thread_json_dump(file_path, content):
+    with open(file_path, 'w', encoding='utf-8') as f:
+        json.dump(content, f, ensure_ascii=False)
+
 class Slot:
+    count = 0
+    previous_server_pk = None
+    previous_date = None
     def __init__(self, server_pk, keyword, product_id, item_id, vendor_item_id, not_update=False):
         self.server_pk = int(server_pk)
         self.keyword = keyword
@@ -19,20 +30,38 @@ class Slot:
         self.item_id = item_id
         self.vendor_item_id = vendor_item_id
         self.not_update = not_update
-
+        if Slot.previous_server_pk != self.server_pk:
+            Slot.count += 1
+        Slot.previous_server_pk = self.server_pk
 
 async def fetch_slots(CONCURRENCY_MAX) -> List[Slot]:    
     # todo: slot 정보를 불러와서 Slot 으로 객체화하여 리스트에 반환    
     def get_param_dict(url):
         params = parse_qs(urlsplit(url).query)
         return {k:v[0] if v else None for k,v in params.items()}
-    data = await get_data_set(CONCURRENCY_MAX)
-    # print(f"갯수 {len(slots)}")
+    data = await get_data_set(CONCURRENCY_MAX)        
+    data = OrderedDict(sorted(data.items(), key=lambda x: len(x[1]), reverse=True))
+    write_executor.submit(thread_json_dump, 'before_processing.json', data)
     object_list = []
     list_chunks = []
-    i = 0
-    while i < len(data):
-        url, val = list(data.items())[i]
+    test_chunk = []
+    test_list_chunks = []
+    i = 0    
+    urls = list(data.keys())    
+    while len(data.items()):
+        url = urls[i]
+        try:
+            value = data[url]
+        except KeyError:
+            continue    
+        if not value:
+            del(data[url])
+            if len(urls) - 1 == i:        
+                i = 0
+            else:
+                i += 1
+            i += 1
+            continue
         try:
             re.match(r'https.+', url).group()
         except AttributeError:
@@ -40,26 +69,35 @@ async def fetch_slots(CONCURRENCY_MAX) -> List[Slot]:
         residue = re.sub(r'\?.+', '', url)
         res = get_param_dict(url)
         product_id = re.sub(r'[^0-9]+', '', residue)
-        res['productId'] = product_id        
+        res['productId'] = product_id
         if len(object_list) >= CONCURRENCY_MAX:
             list_chunks.append(object_list)
+            test_list_chunks.append(test_chunk)
+            test_chunk = []
             object_list = []
-        val = val.pop()
+        
+        val = data[url].pop()    
         id = list(val.keys())
         keyword = list(val.values())
         try:
             object_list.append(Slot(server_pk=id[0], product_id=res['productId'], item_id=res["itemId"], keyword=keyword[0], vendor_item_id=res["vendorItemId"]))
+            test_chunk.append({"server_pk": id[0], "product_id": res['productId'], "item_id": res["itemId"], "vendor_item_id": res["vendorItemId"], "keyword": keyword[0]})
             print(id[0], keyword[0], res)
         except KeyError:            
             object_list.append(Slot(server_pk=id[0], product_id=res['productId'], item_id=res["vendorItemId"], keyword=keyword[0], vendor_item_id=res["vendorItemId"]))
+            test_chunk.append({"server_pk": id[0], "product_id": res['productId'], "item_id": res["vendorItemId"], "vendor_item_id": res["vendorItemId"], "keyword": keyword[0]})
             print(id[0], keyword[0], res)
-        i += 1
+    test_list_chunks.append(test_chunk)
+    list_chunks.append(object_list)
+    write_executor.submit(thread_json_dump, 'after_processing.json', test_list_chunks)
     random.shuffle(list_chunks)
     return list_chunks
 
 
 def preprocess(result):
     vendor_dict = {}
+    #hard coding for 찰보리빵    
+    result.append({'p_url': 'https://www.coupang.com/vp/products/7054578165?itemId=17475148376&vendorItemId=84642758010&isAddedCart=', 'Keyword': '찰보리빵', 'id': 9999999})
     for item in result:
         try:
             item['p_url'] = re.sub(r' + ', '', item['p_url'])
